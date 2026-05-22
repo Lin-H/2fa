0176227745a08bd0d71e78cf2bae0ede0cdc8359
Lin-H <lh04667649@qq.com>
2026-05-23 03:30:33 +0800

    feat: change to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Link, Route, Switch } from 'wouter';
+import { Router, Link, Route, Switch } from 'wouter';
+import { useHashLocation } from 'wouter/use-hash-location';
 import './App.css';
 import setting from '@/assets/setting.svg';
 import Code from './components/Code';
@@ -6,18 +7,20 @@ import Setting from './pages/Setting';
 
 function App() {
   return (
-    <div className="container">
-      <h1 className="title">
-        Authenticator
-        <Link href="/setting" style={{ display: 'inline-flex' }}>
-          <img src={setting} className="setting" alt="setting" />
-        </Link>
-      </h1>
-      <Switch>
-        <Route path="/" component={Code} />
-        <Route path="/setting" component={Setting} />
-      </Switch>
-    </div>
+    <Router hook={useHashLocation}>
+      <div className="container">
+        <h1 className="title">
+          Authenticator
+          <Link href="/setting" style={{ display: 'inline-flex' }}>
+            <img src={setting} className="setting" alt="setting" />
+          </Link>
+        </h1>
+        <Switch>
+          <Route path="/" component={Code} />
+          <Route path="/setting" component={Setting} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
